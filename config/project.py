030c3e6e1ebef885673d0d380c036e0b9f8a1f25
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-20 13:53:41 +0200

    more stuff

---
+++ config/project.py
@@ -0,0 +1,6 @@
+name = "demos-splunk"
+description_short = "demos for the splunk big data platform"
+keywords = [
+    "splunk",
+    "demos",
+]
---
+++ config/python.py
@@ -0,0 +1,5 @@
+make_requires = [
+    "pymakehelper",
+    "pydmt",
+    "pyclassifiers",
+]
